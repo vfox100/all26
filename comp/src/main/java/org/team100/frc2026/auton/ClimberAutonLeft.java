2b4350859122bd1120a25cbb18401250510a1452
Harpsicord <harperangelows@gmail.com>
2026-03-01 10:13:01 -0800

    Update ClimberAutonLeft.java

--- comp/src/main/java/org/team100/frc2026/auton/ClimberAutonLeft.java
+++ comp/src/main/java/org/team100/frc2026/auton/ClimberAutonLeft.java
@@ -23,7 +23,7 @@ import org.team100.lib.trajectory.path.PathSE2Factory;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class BClimberAutonLeft implements AnnotatedCommand {
+public class ClimberAutonLeft implements AnnotatedCommand {
     private final LoggerFactory log;
     private final ControllerSE2 controller;
     private final Machinery machinery;
@@ -32,7 +32,7 @@ public class BClimberAutonLeft implements AnnotatedCommand {
     private final PathSE2Factory pathFactory;
     private final TrajectorySE2Planner planner;
 
-    public BClimberAutonLeft(
+    public ClimberAutonLeft(
             LoggerFactory parent,
             SwerveKinodynamics kinodynamics,
             ControllerSE2 controller,
@@ -48,7 +48,7 @@ public class BClimberAutonLeft implements AnnotatedCommand {
 
     @Override
     public String name() {
-        return "BClimber Auton Left";
+        return "Climber Auton Left";
     }
 
     TrajectorySE2 t1(Pose2d startingPose) {
