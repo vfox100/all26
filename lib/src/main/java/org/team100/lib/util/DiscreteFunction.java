cb63754b8c37f3c395ea9246a3ed4af8603c89b7
Joel Truher <joel@truher.org>
2026-03-04 08:36:14 -0800

    function inverse

--- lib/src/main/java/org/team100/lib/util/DiscreteFunction.java
+++ lib/src/main/java/org/team100/lib/util/DiscreteFunction.java
@@ -1,11 +1,12 @@
 package org.team100.lib.util;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /** Samples from a continuous function, with a payload for each point. */
 public class DiscreteFunction<V> {
-    private record Point<V>(double x, double y, V p) {
+    public record Point<V>(double x, double y, V p) {
     }
 
     private final List<Point<V>> m_points;
@@ -23,9 +24,32 @@ public class DiscreteFunction<V> {
     }
 
     // /** Requires monotonicity. */
-    // public DiscreteFunction<V> invert() {
-        
+    public DiscreteFunction<V> inverse() {
+        checkMonotonicity();
+        DiscreteFunction<V> inverse = new DiscreteFunction<>();
+        for (Point<V> p : m_points) {
+            inverse.put(p.y, p.x, p.p());
+        }
+        return inverse;
+    }
 
-    // }
+    /** Throw if the function is not monotonic */
+    private void checkMonotonicity() {
+        // sort by x
+        m_points.sort(Comparator.comparing(Point::x));
+        boolean increasing = false;
+        boolean decreasing = false;
+        for (int i = 0; i < m_points.size() - 1; ++i) {
+            Point<V> p0 = m_points.get(i);
+            Point<V> p1 = m_points.get(i + 1);
+            if (p0.y >= p1.y)
+                increasing = true;
+            if (p0.y <= p1.y)
+                decreasing = true;
+        }
+        if (increasing && decreasing) {
+            throw new IllegalStateException("non-monotonic function is not invertible");
+        }
+    }
 
 }
--- lib/src/test/java/org/team100/lib/util/DiscreteFunctionTest.java
+++ lib/src/test/java/org/team100/lib/util/DiscreteFunctionTest.java
@@ -1,14 +1,57 @@
 package org.team100.lib.util;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
+import org.team100.lib.util.DiscreteFunction.Point;
 
 public class DiscreteFunctionTest {
+    private static final boolean DEBUG = true;
+
     @Test
     void testMonotonic() {
         DiscreteFunction<String> f = new DiscreteFunction<>();
         f.put(0.0, 0.0, "a");
-        f.put(1.0, 1.0, "b");
-        f.put(2.0, 2.0, "c");
+        f.put(1.0, 10.0, "b");
+        f.put(2.0, 20.0, "c");
+        if (DEBUG)
+            System.out.println("F");
+        dump(f);
+        DiscreteFunction<String> finv = f.inverse();
+        if (DEBUG)
+            System.out.println("Finv");
+        dump(finv);
+    }
+
+    @Test
+    void testNonMonotonic() {
+        DiscreteFunction<String> f = new DiscreteFunction<>();
+        f.put(0.0, 0.0, "a");
+        f.put(1.0, 10.0, "b");
+        f.put(2.0, 0.0, "c");
+        if (DEBUG)
+            System.out.println("F");
+        dump(f);
+        assertThrows(IllegalStateException.class, f::inverse);
+    }
+
+    @Test
+    void testZeroDerivative() {
+        DiscreteFunction<String> f = new DiscreteFunction<>();
+        f.put(0.0, 0.0, "a");
+        f.put(1.0, 10.0, "b");
+        f.put(2.0, 10.0, "c");
+        if (DEBUG)
+            System.out.println("F");
+        dump(f);
+        assertThrows(IllegalStateException.class, f::inverse);
+    }
+
+    private <V> void dump(DiscreteFunction<V> f) {
+        for (Point<V> p : f.points()) {
+            if (DEBUG)
+                System.out.printf("%s\n", p);
+        }
     }
 
 }
